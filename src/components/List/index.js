04890cac56dc7bc46025430ffc70419df6632312
galahad <galahad.serpstat@gmail.com>
2022-09-08 15:49:37 +0300

    Add list component

---
+++ src/components/List/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const List = () => {
+  return (
+    <ul>
+      <li>Пройти курс react</li>
+      <li>Написать компонент ToDo</li>
+      <li>Сдать задание</li>
+      <li>Радоваться жизни</li>
+    </ul>
+  )
+}
+
+export default List
